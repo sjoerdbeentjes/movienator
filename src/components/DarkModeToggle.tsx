dd3489ab0f6cfa98f1934fc06aca66ef196cbaba
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2020-01-20 19:37:58 +0100

    add back button

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -4,19 +4,13 @@ import { ReactComponent as SunIcon } from '../icons/sun.svg'
 import styled from '../styled-components';
 
 const Wrapper = styled.div`
-  width: 3em;
-  height: 3rem;
+  width: 4em;
+  height: 4rem;
   position: absolute;
-  right: 0;
-  top: 0;
+  right: 0.5em;
+  top: 0.5rem;
   transition: transform 0.2s;
-
-  svg {
-    stroke: ${props => props.theme.colors.text};
-    width: 100%;
-    height: 100%;
-  }
-
+  
   &:hover {
     transform: rotate(-20deg);
   }
@@ -28,8 +22,14 @@ const IconButton = styled.button`
   cursor: pointer;
   width: 100%;
   height: 100%;
-  padding: 0.75rem;
+  padding: 1rem;
   outline: none;
+  
+  svg {
+    stroke: ${props => props.theme.colors.text};
+    width: 100%;
+    height: 100%;
+  }
 `
 
 const DarkModeToggle = ({ darkMode, setDarkMode }: { darkMode: boolean, setDarkMode: Function }) => (
--- src/components/MovieDetail.tsx
+++ src/components/MovieDetail.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import styled from '../styled-components'
+import { ReactComponent as ArrowLeft } from '../icons/arrow-left.svg'
 import { IMovieDetail } from '../types/movies'
 import { LinkButton } from '../styled-components/Button';
 import MovieCast from './MovieCast'
+import { Link } from 'react-router-dom';
 
 const Wrapper = styled.div`
-  padding: 2rem 0;
+  padding: 5rem 0 2rem;
 `
 
 const Top = styled.main`
@@ -70,70 +72,108 @@ const Links = styled.div`
   }
 `
 
+const BackButton = styled.div`
+  width: 4em;
+  height: 4rem;
+  position: absolute;
+  left: 0.5rem;
+  top: 0.5rem;
+  transition: transform 0.2s;
+  
+  &:hover {
+    transform: translateX(-2px);
+  }
+`
+
+const BackLink = styled(Link)`
+  display: inline-block;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  height: 100%;
+  padding: 1rem;
+  outline: none;
+  
+  svg {
+    stroke: ${props => props.theme.colors.text};
+    width: 100%;
+    height: 100%;
+  }
+`
+
 const MovieDetail = ({ movie }: { movie: IMovieDetail }) => {
   return (
-    <Wrapper>
-      <Top>
-        <Poster>
-          <img src={`https://yst.am${movie.large_cover_image}`} alt=""/>
-        </Poster>
+    <>
+      <BackButton>
+        <BackLink to="/">
+          <ArrowLeft />
+        </BackLink>
+      </BackButton> 
+      <Wrapper>
+        <Top>
 
-        <Content>
-          <ContentSection>
-            <Title>{movie.title}</Title>
-          </ContentSection>
+          <Poster>
+            <img src={`https://yst.am${movie.large_cover_image}`} alt=""/>
+          </Poster>
 
-          <ContentSection>
-            <Description>{movie.description_intro}</Description>
-          </ContentSection>
+          <Content>
+            <ContentSection>
+              <Title>{movie.title}</Title>
+            </ContentSection>
 
-          <ContentSection>
-            <ContentSectionTitle>Ratings:</ContentSectionTitle>
-            <Links>
-              <LinkButton
-                href={`https://www.imdb.com/title/${movie.imdb_code}`}
-                backgroundColor="#F5C518"
-              >
-                IMDb: {movie.rating}
-              </LinkButton>
-            </Links>
-          </ContentSection>
+            <ContentSection>
+              <Description>{movie.description_intro}</Description>
+            </ContentSection>
 
-          <ContentSection>
-            <ContentSectionTitle>Torrents:</ContentSectionTitle>
-            <Links>
-              {movie.torrents.map(torrent => (
+            <ContentSection>
+              <ContentSectionTitle>Ratings:</ContentSectionTitle>
+              <Links>
                 <LinkButton
-                  href={`https://yst.am${torrent.url}`}
-                  key={torrent.hash}
-                  backgroundColor="#0077ff"
-                  color="#fff"
+                  href={`https://www.imdb.com/title/${movie.imdb_code}`}
+                  backgroundColor="#F5C518"
                 >
-                  {torrent.quality} - {torrent.type} ({torrent.size})
+                  IMDb: {movie.rating}
                 </LinkButton>
-              ))}
-            </Links>
-          </ContentSection>
+              </Links>
+            </ContentSection>
 
-          <ContentSection>
-            <ContentSectionTitle>Subtitles:</ContentSectionTitle>
-            <Links>
-              {movie.subs.map(sub => (
-                <LinkButton
-                  href={sub.ZipDownloadLink}
-                  key={sub.SubHash}
-                  backgroundColor="#0077ff"
-                  color="#fff"
-                >
-                  {sub.SubFileName}
-                </LinkButton>
-              ))}
-            </Links>
-          </ContentSection>
-        </Content>
-      </Top>
-      <MovieCast members={movie.cast} />
-    </Wrapper>
+            <ContentSection>
+              <ContentSectionTitle>Torrents:</ContentSectionTitle>
+              <Links>
+                {movie.torrents.map(torrent => (
+                  <LinkButton
+                    href={`https://yst.am${torrent.url}`}
+                    key={torrent.hash}
+                    backgroundColor="#0077ff"
+                    color="#fff"
+                  >
+                    {torrent.quality} - {torrent.type} ({torrent.size})
+                  </LinkButton>
+                ))}
+              </Links>
+            </ContentSection>
+
+            <ContentSection>
+              <ContentSectionTitle>Subtitles:</ContentSectionTitle>
+              <Links>
+                {movie.subs.map(sub => (
+                  <LinkButton
+                    href={sub.ZipDownloadLink}
+                    key={sub.SubHash}
+                    backgroundColor="#0077ff"
+                    color="#fff"
+                  >
+                    {sub.SubFileName}
+                  </LinkButton>
+                ))}
+              </Links>
+            </ContentSection>
+          </Content>
+        </Top>
+        <MovieCast members={movie.cast} />
+      </Wrapper>
+    </>
   )
 };
 
